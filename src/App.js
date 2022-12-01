8a1c3b6ead8209357893c033f4271ff52e018550
AnandMalviya930 <95901670+AnandMalviya930@users.noreply.github.com>
2022-12-01 18:56:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import LoanCalculator from './LoanCalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='back'>
+      <header className="App-header">
+        <LoanCalculator />
+      </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
